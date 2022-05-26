2d15c0c84884ca7c44301036ffdab23b4d811ea3
Sarah West <west0883@umn.edu>
2022-05-26 09:39:39 -0500

    Separate registration into the two levels of functions I decided I wanted to use.

---
+++ pick_reference_day.m
@@ -0,0 +1,15 @@
+% pick_reference_day.m
+% Sarah West
+% 8/23/21
+
+% Loads the "bback.mat" file for each day of each mouse (which is the
+% reference image for the within- and across-stack registrations) and plots them
+% so you can pick what day to use. Called by "registration_pick_reference_day_permouse.m"
+
+function []=pick_reference_day(days_all, dir_exper)
+
+
+
+
+
+end
--- registration_pick_reference_day_permouse.m
+++ registration_pick_reference_day_permouse.m
@@ -2,13 +2,5 @@
 % Sarah West
 % 8/23/21
 
-% Loads the "bback.mat" file for each day of each mouse (which is the
-% reference image for the within- and across-stack registrations 
-
-function []=registration_pick_reference_day_permouse(days_all, dir_exper)
-
-
-
-
-
-end
+% Cyles through all the mice, calls pick_reference_day, saves the day the
+% user picks for registration. Called by main pipeline code
