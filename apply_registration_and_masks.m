65ac7e493a5e9a09b1f53666a65155844225f630
Sarah West <west0883@umn.edu>
2022-05-26 09:40:25 -0500

    Moved PolyDraw.m into this folder because it's called by the masking code; added steps to transformation part of apply_registration_and_masks.m

--- apply_registration_and_masks.m
+++ apply_registration_and_masks.m
@@ -13,43 +13,66 @@
  
 function []=apply_registration_and_masks(days_all, dir_exper)
     
-    % Establish base input and output directories
+    % Establish base input directories
     dir_in_base_data=[dir_exper 'hemodynamics corrected\']; 
     dir_in_base_tforms=[dir_exper 'tforms across days\']; 
     dir_in_masks=[dir_exper 'masks\'];
     
-    % load reference days 
+    % Establish base output directory
+    dir_out_base=[dir_exper 'fully preprocessed stacks/'];
     
+    % Load reference days
+    load([dir_in_base_tforms 'reference_days.mat']);
+    
+    % For each mouse 
     for mousei=1:size(days_all,2)
         mouse=days_all(mousei).mouse;
-
-        % get the list of days for that mouse
+        
+        % Get the list of all days for that mouse
         days_list=days_all(mousei).days; 
 
-        % for each day
+        % For each day
         for dayi=1:size(days_list,1)
             day=days_list(dayi,:); 
             
+            % Create cleaner data input and output directories; Is just a
+            % preference, but might make it easier to edit directories
+            % later if needed.
+            dir_in=[dir_in_base_data mouse '\' day '\']; 
+            dir_out=[dir_out_base mouse '\' day '\']; 
+            
+            % Load the tform for that day. 
+            load([dir_in_base_tforms mouse '\' day '\tform.mat']); 
+            
+            % List the stacks in this day
+            list=dir([dir_in 'hData*.mat']); 
+            
+            % For each stack 
             for stacki=1:size(list,1)
-                stack_number
-            % for each stack 
+                
+                % Get the stack number for naming output files. 
+                stack_number=list(stacki).name(6:7); 
+                
+                % Load stack data. 
+                load([dir_in list(stacki).name]); 
 
-            % ** Apply registration**
+                % ** Apply registration**
 
-            % Load that day's tform
+                % If the tform's empty, then you don't need to register
+                if isempty(tform)==1 
+                    % Do  nothing
+                else
+                    % Else (the tform isn't empty) perform the registration/warp. 
+                    % Use imwarp to tranform
+                    % the current image to align with the reference image using the tranform
+                    % stored in the tform variable. Should be able to apply to all
+                    % images in the 3rd dimension at the same time 
+                     data=imwarp(hData,tform,'OutputView',imref2d(size(hData,1), size(hData,2)));
+                end
+                % ** Apply mask** 
 
-            % if the tform's empty, then you don't need to register
+                % Save resulting stacks. 
 
-            % Else, perform the registration/warp. Use imwarp to tranform
-            % the current image to align with the reference image using the tranform
-            % stored in the tform variable. Should be able to apply to all
-            % images in the 3rd dimension at the same time 
-             data=imwarp(hData,tform,'OutputView',imref2d(size(Rback)));
-
-            % ** Apply mask** 
-            
-            % Save resulting stacks. 
-            
             end
         end 
     end 
