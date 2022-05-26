53497823b38a9ff324935a89919aabebfae75f4e
Sarah West <west0883@umn.edu>
2022-05-26 09:40:39 -0500

    Replacing Preprocessing_Hemo with code that only finds  a representative background image. Reading full tiffs and Hemo correction will be put in Preprocessing.m

---
+++ registration_SaveRepresentativeImages.m
@@ -0,0 +1,90 @@
+% registration_SaveRepresentaiveImages.m
+% Sarah West
+% 8/25/21
+
+% Loads individual images for differentiating between ligh channels, then
+% saves a representaive image for the day. 
+
+function []=registration_SaveRepresentativeImages(dir_dataset, dir_exper, days_all, skip, pixel_rows, pixel_columns, example_image_indices, rep_stacki)
+
+    dir_out_base=[dir_exper 'representative images\'];
+    disp(['Data saved in ' dir_out_base]); 
+    
+    dbase_str='stacks\' ; %name the optical data subfolder and call it dbase_str
+    %usually this is called stacks but other people might call it something
+    %else. It's where the tiff files are stored and must be entered manually
+    
+    % for each mouse
+    for mousei=1:size(days_all,2)
+        % Get the mouse name
+        mouse=days_all(mousei).mouse;
+        
+        % Get the mouse's dataset days
+        days_list=days_all(mousei).days; 
+        
+        % Make skip always odd.
+        if mod(skip,2)==0 
+            %if the remainder of the length of the skip 
+            %(specified as an input argument) after division by 2 is 0
+            skip=skip-1; %subtract 1 from the skip value 
+        end
+        
+        % For each day of the mouse 
+        for dayi=1:size(days_list,1)
+            
+            %create the input directory by day
+            dir_in=[dir_dataset day '\' day 'm' mouse '\' dbase_str]; 
+            
+            % get the day name
+            day=days_list(dayi,:); 
+            
+            % make the output directory by day
+            dir_out=[dir_out_base '\' mouse '\' day '\']; 
+            mkdir(dir_out);
+            
+            % find the list of stacks in that day (so you can find the
+            % first one)
+            list=dir([dir_day '0*.tif']);
+            
+            % Find the stack to use for the representative image.
+            stack_number=list(rep_stacki).name(2:3);   
+            
+            % Assign the input file name 
+            input_fileName=[dir_day list(stacki).name]; 
+            
+            % Assign the output filename
+            output_fileName=[dir_out 'bback' stack_number '.mat'];
+            
+            % find if there is a selected reference image for this day
+            if isfile([dir_out '\bback.mat'])==1 
+                % If it exists, do nothing; don't need to create a new
+                % one
+            else 
+                % If it doesn't exist, then need to create a new one
+                  
+                
+                % Select 2 sequential images to read; are the first two
+                % after the skip; Need 2 to confirm which channel is which.
+                image_indices=[skip, skip+1]; 
+                
+                % Read those two images
+                im_list=tiffreadAltered_SCA(fileName, image_indices, 'ReadUnknownTags',1);
+                
+                im1=im_list(1).data;
+                im2=im_list(2).data; 
+                
+                % Compare the brightness of the two images; 
+                first_image_channel = determine_channel(im1, im2, pixel_rows, pixel_cols);
+                
+                if first_image_channel=='b'
+                   bback=im1; 
+                elseif first_image_channel=='v'
+                   bback=im2;
+                end
+                
+                % Save the representative image
+                save([dir_out 'bback.mat'], 'bback');
+            end 
+        end 
+    end
+end 
