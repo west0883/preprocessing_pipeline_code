645f859647627222cd83c3c5453aca7267140b16
Sarah West <west0883@umn.edu>
2022-05-26 09:39:35 -0500

    Added separate code/function to pick the day you want to use for each mouse as the reference for across-day registration.

---
+++ registration_pick_reference_day_permouse.m
@@ -0,0 +1,6 @@
+% registration_pick_reference_day_permouse.m
+% Sarah West
+% 8/23/21
+
+% Loads the "bback.mat" file for each day of each mouse (which is the
+% reference image for the within- and across-stack registrations 
