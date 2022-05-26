d92ea0843b5bdd20a9460f3c970448e051825aec
Sarah West <west0883@umn.edu>
2022-05-26 09:39:37 -0500

    Added function formatting

--- registration_pick_reference_day_permouse.m
+++ registration_pick_reference_day_permouse.m
@@ -4,3 +4,11 @@
 
 % Loads the "bback.mat" file for each day of each mouse (which is the
 % reference image for the within- and across-stack registrations 
+
+function []=registration_pick_reference_day_permouse(days_all, dir_exper)
+
+
+
+
+
+end
