e76bc1b0a2d6fbdf9a2da42533cfee5ebed39197
Sarah West <west0883@umn.edu>
2022-05-26 09:39:40 -0500

    Started layout of how I want this function to work.

--- registration_pick_reference_day_permouse.m
+++ registration_pick_reference_day_permouse.m
@@ -4,3 +4,22 @@
 
 % Cyles through all the mice, calls pick_reference_day, saves the day the
 % user picks for registration. Called by main pipeline code
+
+function []=registration_pick_reference_day_permouse(days_all, dir_exper)
+
+    % for each mouse 
+    for mousei=1:size(days_all,2)
+        
+        % find the directories and images for the pick_reference_day function
+        
+        % run the pick_reference_day function
+        
+        % hold onto the user output (of which day should be used as
+        % reference)
+       
+        
+    end 
+    
+    % save the user outputs/reference days as a variable
+
+end 
