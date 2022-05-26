027f9ea7c39eacc49e1c20f525b4bd612b9b14fa
Sarah West <west0883@umn.edu>
2022-05-26 09:44:32 -0500

    Add section that checks image dimensions.

--- Preprocessing.m
+++ Preprocessing.m
@@ -229,10 +229,58 @@ function []=Preprocessing(parameters)
                     end 
                 end
                 
-               
-                % Reshape data into a 2D matrix (total pixels x frames) for
+                % ***Check if image of stack are the right size. Use just
+                % the first frame. ***
+                yDim = size(bData, 1);
+                xDim = size(bData, 2); 
+                
+                % Check fist dimension
+                if yDim ~= parameters.pixels(1) 
+                    
+                    % Only do this step if the pixels are 1 more than
+                    % expected, else display a message and continue to next
+                    % stack.
+                    if yDim - parameters.pixels(1) == 1
+                        
+                        disp('Fixing image y dimension.'); 
+                        
+                        % Remove very last pixel of blue data.
+                        bData(end, :, :) = []; 
+                        
+                        % If two channels, repeat with violetdata
+                        if parameters.channelNumber ==2
+                            vData(end, :, :) = [];
+                        end 
+                    else 
+                        disp(['First dimension wrong size, mouse ' mouse ', day ' day ', stack ' stack_number ]);  
+                        continue
+                    end
+                    
+                end
+                
+                % Check second dimension
+                if xDim ~= parameters.pixels(2)
+                    if xDim - parameters.pixels(1) == 1
+                        
+                        disp('Fixing image x dimension.'); 
+                        
+                        % Remove very last pixel of blue data.
+                        bData(:, end, :) = []; 
+                        
+                        % If two channels, repeat with violetdata
+                        if parameters.channelNumber ==2
+                            vData(:, end, :) = [];
+                        end 
+                        
+                    else 
+                        disp(['Second dimension wrong size, mouse ' mouse ', day ' day ', stack ' stack_number ]);  
+                        continue 
+                    end
+                end
+                
+                % *** Reshape data into a 2D matrix (total pixels x frames) for
                 % applying the mask, regressions, and the lowpass filter. Overwrite the variable
-                % so you don't take up excess memory. 
+                % so you don't take up excess memory. ***
                 bData=reshape(bData, yDim*xDim, frames);
                 
                 % If more than 1 channel, do for violet channel as well
