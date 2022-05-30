a4c7f1748214d69d5ead9054fc8fdcaef53837c8
Sarah West <west0883@umn.edu>
2022-05-29 20:50:05 -0500

    Speed up

--- RegisterStack_WithPreviousDFTShifts.m
+++ RegisterStack_WithPreviousDFTShifts.m
@@ -23,47 +23,31 @@
     
 function [registered_stack] =RegisterStack_WithPreviousDFTShifts(tforms, stack_to_register, usfac) 
     
-    % Initialize output registered stack
-    registered_stack=NaN(size(stack_to_register)); 
+    % Create a Fourier-transformed version of the image you want to
+    % register, keeping with the name of the variable used in the
+    % dftregistration code to try to keep comparisons simple
+    buf2ft=fft2(im);  
     
-    % Find number of frames of stack
-    frames=size(stack_to_register,3); 
+    % find dimensions of images needed for the calulations
+    % (from dftregistration.m)
+    [nr,nc]=size(buf2ft,[1 2]);
+    Nr = ifftshift(-fix(nr/2):ceil(nr/2)-1);
+    Nc = ifftshift(-fix(nc/2):ceil(nc/2)-1);
+    [Nc,Nr] = meshgrid(Nc,Nr);
     
-    % For each frame 
-    parfor t=1:frames
-       % Grab variables from tforms for this frame.
-       diffphase=tforms(1,t);
-       row_shift=tforms(2,t); 
-       col_shift=tforms(3,t);
-    
-       % Grab the image you want to work with 
-       im=stack_to_register(:,:,t); 
-       
-       % Create a Fourier-transformed version of the image you want to
-       % register, keeping with the name of the variable used in the
-       % dftregistration code to try to keep comparisons simple
-       buf2ft=fft2(im);  
-
-        % find dimensions of images needed for the calulations
-        % (from dftregistration.m)
-        [nr,nc]=size(buf2ft);
-        Nr = ifftshift(-fix(nr/2):ceil(nr/2)-1);
-        Nc = ifftshift(-fix(nc/2):ceil(nc/2)-1);
-
         % calculate violet registered image
         if (usfac > 0)
-            [Nc,Nr] = meshgrid(Nc,Nr);
-            vGreg = buf2ft.*exp(1i*2*pi*(-row_shift*Nr/nr-col_shift*Nc/nc));
-            vGreg = vGreg*exp(1i*diffphase);
+            vGreg = buf2ft.*exp(1i*2*pi*(-tforms(2, :)*Nr/nr-tforms(3, :)*Nc/nc));
+            vGreg = vGreg*exp(1i*tforms(1, :));
+
         elseif (usfac == 0)
-            vGreg = buf2ft*exp(1i*diffphase);
+            vGreg = buf2ft*exp(1i*tforms(1, :));
+            
         end
 
         %Get the absolute value of the inverse fourier transform of the
         %registered images.
-        registered_im = abs(ifft2(vGreg));
+        registered_stack = abs(ifft2(vGreg));
         
-        % Put registered image into variable holding registered stack.
-        registered_stack(:,:,t)=registered_im;
-    end
+        
 end 
