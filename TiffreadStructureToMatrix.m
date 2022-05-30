3dc861602d6c1f1250048343bd046c9963e49465
Sarah West <west0883@umn.edu>
2022-05-29 20:52:40 -0500

    Speed up.

--- TiffreadStructureToMatrix.m
+++ TiffreadStructureToMatrix.m
@@ -5,7 +5,7 @@
 % Takes the output structure from tiffread functions and turns the .data
 % fields into one big matrix. 
 
-function [data_matrix]=TiffreadStructureToMatrix(data_structure,frames_list)
+function [data_matrix]=TiffreadStructureToMatrix(data_structure, frames_list)
 
 % Inputs:
 % data_structure -- a structure of images outputted by tiffread
@@ -18,20 +18,7 @@ function [data_matrix]=TiffreadStructureToMatrix(data_structure,frames_list)
     % Use the first frame of the structure to determine image dimensions 
     [yDim, xDim]=size(data_structure(1).data); 
     
-    % Use the size of the frames list to determine how many frames the data matrix will have
-    frames=length(frames_list); 
-    
-    % Initialize size of data_matrix
-    data_matrix=NaN(yDim, xDim, frames); 
-    
-    % For each frame, put it into the data_matrix at appropriate place, converting to double precision 
-    parfor framei=1:frames
-        
-        % Make things a bit easier by defining your frame numbers. 
-        frame_structure=frames_list(framei); 
-      
-        % Now, convert the frames to double precision (from uint16) and move them. 
-        data_matrix(:,:,framei)=data_structure(frame_structure).data; 
-    end 
+    data_matrix = cell2mat({data_structure(frames_list).data});
+    data_matrix = reshape(data_matrix, yDim, xDim, []);
 
 end 
